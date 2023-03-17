787028a651ceb450f38d62ea457b3a6181aab967
Dimitri Rosenberg <dimitri@lockvoid.com>
2023-03-17 12:58:42 +0200

    Add polyfills

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  events: require.resolve('events'),
+  fs: require.resolve('react-native-level-fs'),
+  path: require.resolve('path-browserify'),
+  stream:  require.resolve('stream-browserify'),
+}
